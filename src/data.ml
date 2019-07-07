371eaba33bb91b6989a6eb7fd5253c28702cbb8d
gabrielhdt <gabrielhondet@gmail.com>
2019-07-07 10:55:58 +0200

    some comments and pretty printing

--- src/data.ml
+++ src/data.ml
@@ -1,15 +1,22 @@
 open Core
 open Extra
 
+(** Type of data and statistics. *)
 type t =
-  { cardinal : int }
+  { sym_cardinal : int
+  (** Number of symbols declared. *) }
   [@@deriving yojson]
 
 let compile = Compile.compile true
 
+(** [of_file f] computes statistics on rules of file [f]. *)
 let of_file : string -> t = fun fname ->
   let mp = Files.module_path fname in
   compile mp ;
   let sign = Files.PathMap.find mp Sign.(Timed.(!loaded)) in
-  let cardinal = StrMap.cardinal Timed.(!(sign.sign_symbols)) in
-  { cardinal }
+  let sym_cardinal = StrMap.cardinal Timed.(!(sign.sign_symbols)) in
+  { sym_cardinal }
+
+(** [pp f d] pretty prints data [d] to formatter [f]. *)
+let pp : Format.formatter -> t -> unit = fun fmt d ->
+  d |> to_yojson |> Yojson.Safe.pretty_print fmt
--- src/resystance.ml
+++ src/resystance.ml
@@ -8,8 +8,7 @@ let _ =
   let files = ref [] in
   Arg.parse spec (fun s -> files := s :: !files) usage ;
   let stats = List.map Data.of_file (!files) in
-  let dy = List.map Data.to_yojson stats in
   List.iter
-    (Yojson.Safe.pretty_print Format.std_formatter)
-    dy ;
+    (Data.pp Format.std_formatter)
+    stats ;
   print_newline ()
