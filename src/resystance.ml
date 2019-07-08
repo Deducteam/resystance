e2a21874fe116d9030fb36c1333448795632568e
Gabriel Hondet <gabrielhondet@gmail.com>
2019-07-08 13:09:48 +0200

    cleaner output

--- src/resystance.ml
+++ src/resystance.ml
@@ -1,9 +1,12 @@
+open Core
+
 let spec =
   let sp = Arg.align
              [ ] in
   List.sort (fun (f1, _, _) (f2, _, _) -> String.compare f1 f2) sp
 
 let _ =
+  Console.set_default_verbose 0 ;
   let usage = Printf.sprintf "Usage: %s [OPTIONS] [FILES]" Sys.argv.(0) in
   let files = ref [] in
   Arg.parse spec (fun s -> files := s :: !files) usage ;
