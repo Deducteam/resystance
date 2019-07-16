49ca415a6c7d45fad83a23132053e2cbab2a8fba
Gabriel Hondet <gabrielhondet@gmail.com>
2019-07-16 11:45:04 +0200

    output as csv

--- src/resystance.ml
+++ src/resystance.ml
@@ -1,13 +1,21 @@
 open Core
 
+module F = Format
+
 (** Whether to output as a csv line. *)
 let csv : bool ref = ref false
+
+(** Whether to output the stats of each file as csv lines. *)
+let separate : bool ref = ref false
 
 let spec =
   let sp = Arg.align
       [ ( "--csv"
         , Arg.Set csv
-        , " Output as a csv line" ) ] in
+        , " Output as a csv line" )
+      ; ( "--separate"
+        , Arg.Set separate
+        , " One csv output line per file" ) ] in
   List.sort (fun (f1, _, _) (f2, _, _) -> String.compare f1 f2) sp
 
 let _ =
@@ -16,7 +24,9 @@ let _ =
   let files = ref [] in
   Arg.parse spec (fun s -> files := s :: !files) usage ;
   let stats = List.map Data.of_file (!files) in
-  let final = Data.merge stats in
-  let pp = if !csv then Data.pp_csv else Data.pp in
-  pp Format.std_formatter final ;
-  Format.print_newline ()
+  let pp = if !csv || !separate then Data.pp_csv else Data.pp in
+  let ppf = F.std_formatter in
+  if !separate then
+    List.iter (F.fprintf ppf "%a\n" pp) stats
+  else
+    F.fprintf ppf "%a\n" pp (Data.merge stats)
