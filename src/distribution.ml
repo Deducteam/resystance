cfc1d171b005b142811b7271f213be0675f3fb44
gabrielhdt <gabrielhondet@gmail.com>
2019-08-27 09:01:17 +0200

    no need for record

--- src/distribution.ml
+++ src/distribution.ml
@@ -1,40 +1,35 @@
 (** Manipulation statistical distributions. *)
 
 (** Type of a distribution of integers. *)
-type t =
-  { cardinal : int
-  (** Number of values in this distribution. *)
-  ; values : int array
-  (** Values of the distribution. *) }
+type t = int array
 
 (** [distribution d] sorts dataset [d]. *)
 let distribution : t -> t = fun d ->
-  Array.fast_sort (-) d.values ;
+  Array.fast_sort (-) d ;
   d
 
 (** [of_list l] creates a distribution from a list. *)
 let of_list : int list -> t = fun l ->
-  distribution { values = Array.of_list l ; cardinal = List.length l }
+  distribution @@ Array.of_list l
 
 (** [init] creates an empty distribution with value [x]. *)
-let empty : t =
-  { cardinal = 0 ; values = [||] }
+let empty : t = [||]
 
 (** [merge t u] merges two distributions [t] and [u] into one. *)
 let merge : t -> t -> t = fun a b ->
-  distribution { cardinal = a.cardinal + b.cardinal
-               ; values = Array.append a.values b.values }
+  distribution (Array.append a b)
 
-let percentile : int -> t -> int = fun k { cardinal ; values ; _ } ->
-  if cardinal = 0 then 0 else
-  let index = int_of_float ((float_of_int (k * cardinal)) /. 100.) in
-  values.(index)
+let percentile : int -> t -> int = fun k d ->
+  if d = [||] then 0 else
+  let card = Array.length d in
+  let index = int_of_float ((float_of_int (k * card)) /. 100.) in
+  d.(index)
 
 let average : t -> float = fun d ->
-  (float_of_int (Array.fold_left (+) 0 d.values)) /. (float_of_int d.cardinal)
+  (float_of_int (Array.fold_left (+) 0 d)) /. (float_of_int (Array.length d))
 
 (** [sd d] computes the standard deviation of [d]. *)
-let sd : t -> float = fun ({ values ; _ } as d) ->
+let sd : t -> float = fun d ->
   let avg = average d in
-  let sqavg = average { d with values = Array.map (fun x -> x * x) values } in
+  let sqavg = average (Array.map (fun x -> x * x) d) in
   sqrt @@ sqavg -. avg *. avg
