a508178429e75cdbc1266581b584ec3e0ee2d2fb
gabrielhdt <gabrielhondet@gmail.com>
2019-10-06 09:13:15 +0200

    [+] interface for unification

---
+++ src/unification.mli
@@ -0,0 +1,33 @@
+(** Syntactic unification operating on Dk3 terms.
+
+    The variables of the unification are rewriting variables, also called
+    pattern variables, built with {!constructor:Core.Terms.Patt}. *)
+
+open Core
+
+(** {2 Variables} *)
+
+val rename : Terms.term -> Terms.term
+(** [rename t] returns term [t] with its (rewrite) variables with
+    fresh names (non linearity is kept). *)
+
+(** {2 Substitutions} *)
+
+type substitution
+(** Substitutions from pattern variables to terms. *)
+
+val pp_subst : Format.formatter -> substitution -> unit
+(** [pp_subst f s] pretty prints substitution [s] to formatter [f]. *)
+
+val lift : substitution -> Terms.term -> Terms.term
+(** [lift s t] applies substitution [s] to term [t]. *)
+
+(** {2 Unification} *)
+
+exception CantUnify
+(** Raised in case of unification failure. *)
+
+val unify : Terms.term -> Terms.term -> substitution
+(** [unify t u] returns a substitution [s] such that [ts = us].
+
+    @raise CantUnify if [t] and [u] are not unifyable. *)
