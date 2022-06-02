a3113f88322fbc66b18ce9cebf37d5741eb8529f
LorisCros <79654106+LorisCros@users.noreply.github.com>
2022-06-02 14:18:52 +0200

    Update Algo_completion.mli

--- src/Algo_completion.mli
+++ src/Algo_completion.mli
@@ -14,9 +14,9 @@ fonctions extension_A et extension_AC qui prennent en compte respectivement les 
 fonction extension prend en argument une liste de règles et donne l’extension globale
 de celle-ci : {extension_AC(règles)}U{extension_A(règles)}U{règles}*)
 
-val extension_A : Term.sym* Term.term*Term.term* Term.rule -> (Term.sym* Term.term*Term.term* Term.rule) list
+val extension_A : (Term.sym* Term.term*Term.term* Term.rule) list -> (Term.sym* Term.term*Term.term* Term.rule) list
 
-val extension_AC : Term.sym* Term.term*Term.term* Term.rule -> (Term.sym* Term.term*Term.term* Term.rule) list
+val extension_AC : (Term.sym* Term.term*Term.term* Term.rule) list -> (Term.sym* Term.term*Term.term* Term.rule) list
 
 (*L’algorithme s’appuie sur le remplissage itératif d’un ensemble nommé Pairs.
 Ce remplissage se fait en suivant certaines règles. Ces étapes sont automatisées à
